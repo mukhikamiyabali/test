c6a1caf5b548c21d6740b8937680d643399c8612
mukhikamiyabali <155345773+mukhikamiyabali@users.noreply.github.com>
2024-01-11 12:17:13 +0000

    11

---
+++ 02_variables.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3) // code readability should be high
+
+// console.log("Hitesh")
+
+var name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+let symbol = "%"
+
+var name = "mukhi"
+age = "50"
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+console.log( typeof symbol);
+console.log(age);
+console.log(name);
+console.log(typeof symbol); // undefined
+console.log(typeof Boolean); // object
